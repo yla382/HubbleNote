60f98f981e5b835a61ecd0b94472761c6eba69fa
yla382 <yoonhlee0831@gmail.com>
2025-03-05 18:56:54 -0800

    add editor styling TODO: figure out linenumber missing in editor

--- src/renderer/src/hooks/useMarkDownEditor.tsx
+++ src/renderer/src/hooks/useMarkDownEditor.tsx
@@ -1,25 +1,55 @@
-import { EditorView, basicSetup } from "@codemirror/basic-setup";
-import { history } from '@codemirror/history';
-import { markdown } from "@codemirror/lang-markdown";
+import { defaultKeymap } from '@codemirror/commands';
+import { highlightActiveLineGutter, lineNumbers } from '@codemirror/gutter';
+import { defaultHighlightStyle, HighlightStyle, tags } from '@codemirror/highlight';
+import { history, historyKeymap } from '@codemirror/history';
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { indentOnInput } from '@codemirror/language';
+import { languages } from '@codemirror/language-data';
+import { bracketMatching } from '@codemirror/matchbrackets';
 import { EditorState } from "@codemirror/state";
+import { oneDark } from '@codemirror/theme-one-dark';
+import { EditorView, keymap } from '@codemirror/view';
 import { saveNoteAtom, selectedNoteAtom } from "@renderer/store";
 import { useAtomValue, useSetAtom } from "jotai";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+
+
+export const transparentTheme = EditorView.theme({
+    '&': {
+      backgroundColor: 'transparent !important',
+      height: '100%'
+    }
+  })
+
+const syntaxHighlighting = HighlightStyle.define([
+    {
+    tag: tags.heading1,
+    fontSize: '1.6em',
+    fontWeight: 'bold'
+    },
+    {
+    tag: tags.heading2,
+    fontSize: '1.4em',
+    fontWeight: 'bold'
+    },
+    {
+    tag: tags.heading3,
+    fontSize: '1.2em',
+    fontWeight: 'bold'
+    }
+])
 
 export const useMarkdownEditor = () => {
     const selectedNote = useAtomValue(selectedNoteAtom);
     const saveNote = useSetAtom(saveNoteAtom);
     const editorRef = useRef<EditorView | null>(null);
     const editorContainerRef = useRef<HTMLDivElement | null>(null);
-    const [editorView, setEditorView] = useState<EditorView>()
-
+    
     const handleAutoSaving = async (content: string) => {
         if (!selectedNote) return;
         // Save the note content to disk
         await saveNote(content);
     };
-
-    console.log(selectedNote)
 
     // Initialize the editor view only once
     useEffect(() => {
@@ -29,15 +59,28 @@ export const useMarkdownEditor = () => {
         const startState = EditorState.create({
             doc: selectedNote.content,
             extensions: [
-                basicSetup,
+                keymap.of([...defaultKeymap, ...historyKeymap]),
                 history(),
-                markdown(),
+                lineNumbers(),
+                indentOnInput(),
+                bracketMatching(),
+                highlightActiveLineGutter(),
+                markdown({
+                    base: markdownLanguage,
+                    codeLanguages: languages,
+                    addKeymap: true
+                }),
+                transparentTheme,
+                defaultHighlightStyle.fallback,
+                syntaxHighlighting,
+                oneDark,
+                EditorView.lineWrapping,
                 EditorView.updateListener.of((update) => {
                     if (update.docChanged) {
                         const content = update.state.doc.toString();
                         handleAutoSaving(content);
                     }
-                }),
+                })
             ],
         });
 
