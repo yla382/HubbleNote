50799c749abea58eb94b9fb8980bc37fb28a8efc
yla382 <yoonhlee0831@gmail.com>
2025-03-07 21:36:43 -0800

    add html support in render view

--- src/renderer/src/components/MarkdownRenderView.tsx
+++ src/renderer/src/components/MarkdownRenderView.tsx
@@ -2,16 +2,23 @@ import { selectedNoteAtom } from "@renderer/store";
 import { useAtomValue } from "jotai";
 import { ComponentProps } from "react";
 import ReactMarkdown from "react-markdown";
-import { twMerge } from 'tailwind-merge';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from "remark-gfm";
+import { twMerge } from "tailwind-merge";
 
 export const MarkdownRenderView = ({ className, ...props }: ComponentProps<"div">) => {
-    const selectedNote = useAtomValue(selectedNoteAtom)
+  const selectedNote = useAtomValue(selectedNoteAtom);
 
-    if (!selectedNote) return null
+  if (!selectedNote) return null;
 
-    return (
-        <div className={twMerge("max-w-none p-4 prose prose-invert break-words select-text", className)} {...props}>
-            <ReactMarkdown>{selectedNote.content}</ReactMarkdown>
-        </div>
-    )
-}
+  return (
+    <div
+      className={twMerge("max-w-none p-4 prose prose-invert break-words select-text", className)}
+      {...props}
+    >
+      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+        {selectedNote.content}
+      </ReactMarkdown>
+    </div>
+  );
+};
